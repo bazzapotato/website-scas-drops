fd70e50a533f78b8fda4e5478cbf062ff510c54b
bazzapotato <71957020+bazzapotato@users.noreply.github.com>
2026-08-17 10:43:27 +1000

    Create drop-01.js

---
+++ JS/drops/drop-01.js
@@ -0,0 +1,14 @@
+window.DROPS_DATA = window.DROPS_DATA || [];
+window.DROPS_DATA.push({
+    id: 1,
+    dropNumber: "01",
+    title: "Genesis Graphic Poster Set",
+    status: "archived",
+    releaseDate: "2026-06-01T12:00:00",
+    price: "$30.00",
+    image: "https://" + ["images", "unsplash", "com"].join(".") + "/" + "photo-1579783900882-c0d3dad7b119?auto=format&fit=crop&w=800&q=80",
+    tagline: "Archival foil print series.",
+    description: "Set of 3 screen-printed risograph posters on 300gsm heavyweight archival paper.",
+    buttonText: "View Archive ↗",
+    link: "#"
+});
