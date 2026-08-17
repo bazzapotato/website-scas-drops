edacf549628c1b479cdb331f3a258eb292c3c99c
bazzapotato <71957020+bazzapotato@users.noreply.github.com>
2026-08-17 12:25:16 +1000

    Update drop-01.js

--- JS/drops/drop-01.js
+++ JS/drops/drop-01.js
@@ -1,14 +1,12 @@
-window.DROPS_DATA = window.DROPS_DATA || [];
-window.DROPS_DATA.push({
-    id: 1,
-    dropNumber: "01",
-    title: "group gallery @ playstate newy west",
-    status: "archived",
-    releaseDate: "2022-03-24",
-    price: 
-    image: "~/JS/Drops/image" "01.jpg"
-    tagline: "collaborative gallery event",
-    description: "A collection of personal works shown at playstate supply in newcastle west (now in islington)",
-    buttonText: "View Archive ↗",
-    link: "#"
-});
+module.exports = {
+  id: 1,
+  dropNumber: "01",
+  title: "group gallery @ playstate newy west",
+  status: "archived",
+  releaseDate: "2022-03-24",
+  price: "N/A",
+  tagline: "collaborative gallery event",
+  description: "A collection of personal works shown at playstate supply in newcastle west (now in islington)",
+  buttonText: "View Archive ↗",
+  link: "#"
+};
